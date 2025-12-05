6dd95b94f04dafd557f6ca75984c66bc75ef78bc
Mitchell <mitch.lewsey@gmail.com>
2025-12-05 14:29:14 +0000

    Added test to check init method created empty list

---
+++ tests/test_music_tracker.py
@@ -0,0 +1,12 @@
+from lib.music_tracker import *
+import pytest
+
+"""
+On initalising the class
+The empty list is created
+"""
+
+def test_music_list_created_on_init():
+    music_tracker = MusicTracker()
+    assert music_tracker.music_list == []
+
